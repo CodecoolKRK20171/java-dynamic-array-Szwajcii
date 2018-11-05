427ee296330d0780fc8e088f4699bb8701ab135d
Szwajcii <d.szwajkos@gmail.com>
2018-11-05 19:14:58 +0100

    Implement constructors and necessary methods.

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,46 @@
 package com.codecool.dynamicArrayDojo;
 
+
+
 // put your code here!
 public class DynamicIntArray {
+
+
+    public DynamicIntArray(){
+        
+    }
+
+
+    public DynamicIntArray(int size){
+        Object[] objArray = new Object[size];
+    }
+
+
+    public int size(){
+        int arrSize = 0;
+
+        return arrSize;
+    }
+
+
+
+    public void add(Object obj){
+
+
+
+    }
+
+
+    public void remove(Object obj){
+
+
+
+    }
+
+
+    public void insert()
+
+
+
 
 }
